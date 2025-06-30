8ad959f72a9172fa9c4a8407c837712477687c93
Fabrice TRAN-XUAN <fabrice.tranxuan@fr.enersys.com>
2025-06-30 18:07:01 +0200

    Solving Docker installation issue

--- commons/commons-docker.sh
+++ commons/commons-docker.sh
@@ -12,20 +12,34 @@ if command -v docker &> /dev/null; then
     echo "Docker is already installed."
 else
     set -e
-    echo "Installing Docker..."
-    sudo apt-get install -y -qq git
+    echo "+-------------------+"
+    echo "| Installing Docker |"
+    echo "+-------------------+"
+    sudo DEBIAN_FRONTEND=noninteractive apt-get install -y -qq git 1>/dev/null
     sudo curl -sL https://raw.githubusercontent.com/ezekeal/scripts/main/docker-pi.sh | bash
     sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
     sudo chmod a+r /etc/apt/keyrings/docker.asc
+    echo "+---------------------------------------------+"
+    echo "| Adding Docker repository to sources list... |"
+    echo "+---------------------------------------------+"
     sudo echo \
     "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
     sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-    sudo apt-get update -y -qq
-    sudo apt-get upgrade -y -qq
-    sudo apt-get install -y -qq docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+    echo "+-------------------------------+"
+    echo "| Updating and upgrading the OS |"
+    echo "+-------------------------------+"
+    sudo DEBIAN_FRONTEND=noninteractive apt-get update -y -qq 1>/dev/null
+    sudo DEBIAN_FRONTEND=noninteractive apt-get upgrade -y -qq 1>/dev/null
+    echo "+---------------------------+"
+    echo "| Installing Docker modules |"
+    echo "+---------------------------+"
+    sudo DEBIAN_FRONTEND=noninteractive apt-get install -y -qq docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin 1>/dev/null
 
 # Setup a 10 MiB rolling log with 3 files
+    echo "+--------------------------------------------+"
+    echo "| Setting up Docker logging configuration... |"
+    echo "+--------------------------------------------+"
 sudo tee /etc/docker/daemon.json > /dev/null << 'EOF_DOCKER_DAEMON'
 {
     "log-driver": "local",
@@ -38,6 +52,9 @@ sudo tee /etc/docker/daemon.json > /dev/null << 'EOF_DOCKER_DAEMON'
 EOF_DOCKER_DAEMON
 
     # Restarts Docker to take in charge the new configuration
+    echo "+------------------------------+"
+    echo "| Restarting Docker service... |"
+    echo "+------------------------------+"
     sudo systemctl restart docker
 
     # Test the Docker installation
