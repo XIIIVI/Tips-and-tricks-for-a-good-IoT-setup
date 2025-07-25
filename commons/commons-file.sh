e3b6aa657dfb8f9392c8e67a0dc162800143aba1
Fabrice TRAN-XUAN <fabrice.tranxuan@fr.enersys.com>
2025-07-25 21:53:09 +0200

    First attempt to create replicated volumes from a re-usable function

--- commons/commons-file.sh
+++ commons/commons-file.sh
@@ -1,49 +1,156 @@
 #!/bin/bash
 
 #
-# split_partition
-# - param1: Size of the second partition in MiB
-# - param2: Dry run mode (true/false)
-split_partition() {
-    local SECOND_SIZE_MIB="$1"
-    local DRY_RUN="$2"
+# create_replicated_disks
+#
+create_replicated_disks() {
+    local LOGIN_ARG="${1}"
+    local PASSWORD_ARG="${2}"
+    local VOLUME_NAME_ARG="${1}"
+    local HOSTNAME_LIST_ARG=("$@")
 
-    log_info "Splitting the partition and adding a new XFS partition of size ${SECOND_SIZE_MIB} MiB"
-
-    # Detect root partition and device
-    local ROOT_PARTITION
-    ROOT_PARTITION=$(findmnt -n -o SOURCE /)  # e.g. /dev/sda1
-
-    local DEVICE
-    DEVICE=$(echo "$ROOT_PARTITION" | sed 's/[0-9]*$//')  # e.g. /dev/sda
-
-    local PARTITION="$ROOT_PARTITION"
-
-    local CURRENT_PART_NUM
-    CURRENT_PART_NUM=$(echo "$ROOT_PARTITION" | grep -o '[0-9]*$')
-    local NEW_PART_NUM=$((CURRENT_PART_NUM + 1))
-
-    local TOTAL_SIZE_MIB
-    TOTAL_SIZE_MIB=$(parted "$DEVICE" --script unit MiB print | awk '/^Disk/ {gsub(/MiB/, "", $3); print int($3)}')
-
-    local RESIZED_SIZE_MIB=$((TOTAL_SIZE_MIB - SECOND_SIZE_MIB))
-    local START_SECOND="${RESIZED_SIZE_MIB}MiB"
-    local END_SECOND="${TOTAL_SIZE_MIB}MiB"
-
-    log_debug "🧠 Plan:"
-    log_warning "\t- Root partition: $PARTITION"
-    log_warning "\t- Base device: $DEVICE"
-    log_warning "\t- Resize ${PARTITION} to ${RESIZED_SIZE_MIB} MiB"
-    log_warning "\t- Create ${DEVICE}${NEW_PART_NUM} from ${START_SECOND} to ${END_SECOND}"
-    log_warning "\t- Format as XFS"
-
-    if [[ "$DRY_RUN" == true ]]; then
-        log_warning "\t- 🧪 Dry-run mode enabled. No changes applied."
+    if [ ${#HOSTNAME_LIST_ARG[@]} -eq 0 ]; then
+        log_error "⚠️ The list of hosts is empty."
     else
-        log_warning "\t- 🚀 Performing changes..."
-        parted "$DEVICE" --script resizepart "$CURRENT_PART_NUM" "${RESIZED_SIZE_MIB}MiB"
-        parted "$DEVICE" --script mkpart primary xfs "$START_SECOND" "$END_SECOND"
-        mkfs.xfs -f "${DEVICE}${NEW_PART_NUM}"
-        log_warning "\t- ✅ Success: New XFS partition created as ${DEVICE}${NEW_PART_NUM}"
-    fi
+         local ETC_HOSTS_FILE=/tmp/etc_hosts.addon
+         local ETC_FSTAB_FILE=/tmp/etc_fstab.addon
+         local GLUSTER_DIR="/gluster/bricks"
+         local COUNTER=1
+         local VOLUME_CREATION_SCRIPT=/tmp/glusterfs_volume_creation_script.sh
+         local MASTER_IP_ADDRESS
+
+         MASTER_IP_ADDRESS=$(host "${HOSTNAME_LIST_ARG[0]}" | awk '/has address/ { print $4 }')
+
+         log_info "Setting up replicated disks with GlusterFS"
+
+         # Clean up temp files
+         rm -Rf "${ETC_HOSTS_FILE}" "${ETC_FSTAB_FILE}" "${VOLUME_CREATION_SCRIPT}"
+         touch "${ETC_HOSTS_FILE}" "${ETC_FSTAB_FILE}" "${VOLUME_CREATION_SCRIPT}"
+
+         # Initializing the script glusterfs_volume_creation_script.sh
+         cat << SCRIPT_EOF >> "${VOLUME_CREATION_SCRIPT}"
+#!/bin/bash
+
+gluster volume create ${VOLUME_NAME_ARG} replica 3 \\
+SCRIPT_EOF
+
+         log_debug "\t- Installing required packages"
+         apt-get install -qq -y dnsutils
+
+         log_debug "\t- Setting up /etc/hosts and /etc/fstab files for GlusterFS volume ${VOLUME_NAME_ARG}"
+         COUNTER=1
+
+         for HOSTNAME in "${HOSTNAME_LIST_ARG[@]}"; do
+             local IP_INDEX
+
+             IP_INDEX=$(host "${HOSTNAME}" | awk '/has address/ { print $4 }')
+
+             echo "${IP_INDEX} ${HOSTNAME}" >>"${ETC_HOSTS_FILE}"
+             echo "${HOSTNAME}:/$VOLUME_NAME_ARG  ${GLUSTER_DIR}/${COUNTER}  glusterfs  defaults,_netdev  0  0" >> "${ETC_FSTAB_FILE}"
+             printf "%s" "${HOSTNAME}:${GLUSTER_DIR}/${COUNTER}/brick " >> "${VOLUME_CREATION_SCRIPT}"
+                
+             COUNTER=$((COUNTER + 1))
+         done
+
+         printf "%s" " force" >> "${VOLUME_CREATION_SCRIPT}"
+
+         unset COUNTER
+
+         log_info "\n\nSetting up GlusterFS on discovered hosts: ${DISCOVERED_IPS[*]}"
+         COUNTER=1
+
+         for HOSTNAME in "${HOSTNAME_LIST_ARG[@]}"; do
+             local IP_INDEX
+
+             IP_INDEX=$(host "${HOSTNAME}" | awk '/has address/ { print $4 }')
+
+             remove_ssh_host "${IP_INDEX}"
+
+             log_warning "\t\t- Declaring the GlusterFS member to /etc/hosts"
+             copy_file_to_host "${LOGIN_ARG}" "${PASSWORD_ARG}" "${IP_INDEX}" "${ETC_HOSTS_FILE}" "/tmp"
+             sshpass -p "${PASSWORD_ARG}" ssh -o StrictHostKeyChecking=no "${LOGIN_ARG}@${IP_INDEX}" "sudo tee -a /etc/hosts < /tmp/$(basename ${ETC_HOSTS_FILE})"
+
+             log_warning "\t\t- Declaring the GlusterFS member to /etc/fstab"
+             copy_file_to_host "${LOGIN_ARG}" "${PASSWORD_ARG}" "${IP_INDEX}" "${ETC_FSTAB_FILE}" "/tmp"
+             sshpass -p "${PASSWORD_ARG}" ssh -o StrictHostKeyChecking=no "${LOGIN_ARG}@${IP_INDEX}" "sudo tee -a /etc/fstab < /tmp/$(basename ${ETC_FSTAB_FILE})"
+
+             log_warning "\t\t- Installing GlusterFS on ${IP_INDEX}"
+             sshpass -p "$PASSWORD_ARG" ssh -o StrictHostKeyChecking=no "$LOGIN_ARG@$IP_INDEX" <<EOF_GLUSTERFS
+    export DEBIAN_FRONTEND=noninteractive
+    export DEBCONF_NOWARNINGS=yes 
+
+    sudo mkdir -p "${GLUSTER_DIR}/${COUNTER}"
+
+    sudo -E apt update -qq
+    sudo -E apt install glusterfs-server -y -qq \
+         -o Dpkg::Progress-Fancy="0" \
+         -o Dpkg::Use-Pty="0"
+    sudo systemctl enable glusterd
+    sudo systemctl start glusterd
+
+    sudo mount -a
+    sudo mkdir -p "${GLUSTER_DIR}/${COUNTER}/brick"
+EOF_GLUSTERFS
+
+             COUNTER=$((COUNTER + 1))
+         done
+
+         unset COUNTER
+
+         # Probing the peers
+         for ((INDEX = 1; INDEX < ${#HOSTNAME_LIST_ARG[@]}; INDEX++)); do
+             local IP_INDEX
+
+             IP_INDEX=$(host "${HOSTNAME_LIST_ARG[INDEX]}" | awk '/has address/ { print $4 }')
+             log_debug "\t\t- Probing host ${IP_INDEX} ..."
+             sshpass -p "${PASSWORD_ARG}" ssh "${LOGIN_ARG}@${MASTER_IP_ADDRESS}" "sudo gluster peer probe ${IP_INDEX}"
+             sleep 5
+         done
+
+         # Waiting for the ppers
+         log_info "⏳ Waiting for peers to join trusted pool..."
+         sleep 5
+         sshpass -p "${PASSWORD_ARG}" ssh "${LOGIN_ARG}@${MASTER_IP_ADDRESS}" "sudo gluster peer status"        
+
+         # Creating the volume
+         log_debug "\t\t- Setting up th GlusterFS volume with the following script: ${VOLUME_CREATION_SCRIPT}"
+         log_warning "\t\t\t- Creating the volume ${VOLUME_NAME_ARG}"
+         copy_file_to_host "${LOGIN_ARG}" "${PASSWORD_ARG}" "${MASTER_IP_ADDRESS}" "${VOLUME_CREATION_SCRIPT}" "/tmp"
+         sshpass -p "${PASSWORD_ARG}" ssh -o StrictHostKeyChecking=no "${LOGIN_ARG}@${MASTER_IP_ADDRESS}" "sudo bash /tmp/$(basename ${VOLUME_CREATION_SCRIPT})"
+         log_warning "\t\t\t- Starting the volume ${VOLUME_NAME_ARG}"
+         sshpass -p "${PASSWORD_ARG}" ssh "${LOGIN_ARG}@${MASTER_IP_ADDRESS}" "sudo gluster volume start ${VOLUME_NAME_ARG}"
+         log_warning "\t\t\t- Status of the volume ${VOLUME_NAME_ARG}"
+         sshpass -p "${PASSWORD_ARG}" ssh "${LOGIN_ARG}@${MASTER_IP_ADDRESS}" "sudo gluster volume status ${VOLUME_NAME_ARG}"
+         log_warning "\t\t\t- Info of the volume ${VOLUME_NAME_ARG}"
+         sshpass -p "${PASSWORD_ARG}" ssh "${LOGIN_ARG}@${MASTER_IP_ADDRESS}" "sudo gluster volume info ${VOLUME_NAME_ARG}"
+         log_warning "\t\t\t- Setup security and authentication for the volume ${VOLUME_NAME_ARG}"
+         sshpass -p "${PASSWORD_ARG}" ssh "${LOGIN_ARG}@${MASTER_IP_ADDRESS}" "sudo gluster volume set ${VOLUME_NAME_ARG} auth.allow $(IFS=, ; echo "${DISCOVERED_IPS[*]}")"
+
+         # Mount the glusterFS volume where applications can access the files
+         log_info "Mounting the GlusterFS volume ${VOLUME_NAME_ARG} on all nodes" 
+         for HOSTNAME in "${HOSTNAME_LIST_ARG[@]}"; do
+             local IP_INDEX
+
+             IP_INDEX=$(host "${HOSTNAME}" | awk '/has address/ { print $4 }')
+            log_debug "\t\t- Mounting the GlusterFS volume ${VOLUME_NAME_ARG} on ${IP_INDEX}"
+            sshpass -p "${PASSWORD_ARG}" ssh -o StrictHostKeyChecking=no "${LOGIN_ARG}@${IP_INDEX}" \
+                    "echo \"localhost:/${VOLUME_NAME_ARG} /mnt glusterfs defaults,_netdev,backupvolfile-server=localhost 0 0\" | sudo tee -a /etc/fstab > /dev/null"
+            sshpass -p "${PASSWORD_ARG}" ssh -o StrictHostKeyChecking=no "${LOGIN_ARG}@${IP_INDEX}" "sudo mount.glusterfs localhost:/${VOLUME_NAME_ARG} /mnt"
+            sshpass -p "${PASSWORD_ARG}" ssh -o StrictHostKeyChecking=no "${LOGIN_ARG}@${IP_INDEX}" "df -Th"
+         done
+
+         # Testing
+         log_debug "\t\t- Testing the GlusterFS volume ${VOLUME_NAME_ARG} on all nodes"
+         sshpass -p "${PASSWORD_ARG}" ssh -o StrictHostKeyChecking=no "${LOGIN_ARG}@${MASTER_IP_ADDRESS}" "echo 'Hello World!' | sudo tee /mnt/test.txt"
+
+         for IP_INDEX in "${HOSTNAME_LIST_ARG[@]}"; do
+             local IP_INDEX
+
+             IP_INDEX=$(host "${HOSTNAME_LIST_ARG[INDEX]}" | awk '/has address/ { print $4 }')
+
+             log_debug "\t\t- Checking test file on host ${IP_INDEX} ..."
+             sshpass -p "${PASSWORD_ARG}" ssh "${LOGIN_ARG}@${IP_INDEX}" "cat /mnt/test.txt"
+             sleep 5
+         done
+     fi
 }
