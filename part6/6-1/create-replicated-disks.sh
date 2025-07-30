b9a606c7e292364a02ff60c6b9fff6fd6f630b04
Fabrice TRAN-XUAN <fabrice.tranxuan@fr.enersys.com>
2025-07-30 08:21:41 +0200

    Simplified the script "create-replicated-disks.sh" with the include of "commons-file.sh"

--- part6/6-1/create-replicated-disks.sh
+++ part6/6-1/create-replicated-disks.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 source "../../commons/commons-cli.sh"
+source "../../commons/commons-file.sh"
 source "../../commons/commons-log.sh"
 source "../../commons/commons-net.sh"
 source "../../commons/commons-ssh.sh"
@@ -69,26 +70,11 @@ main() {
 
     local MANAGER_HOSTNAME_PREFIX="${MANAGER_HOSTNAME_PREFIX:-orchestrator}"
     local VOLUME_NAME="${VOLUME_NAME:-gfs}"
-    DISCOVERED_IPS=()
-    ETC_HOSTS_FILE=/tmp/etc_hosts.addon
-    ETC_FSTAB_FILE=/tmp/etc_fstab.addon
-    GLUSTER_DIR="/gluster/bricks"
+    DISCOVERED_HOSTNAME_LISt=()
     COUNTER=1
-    VOLUME_CREATION_SCRIPT=/tmp/glusterfs_volume_creation_script.sh
-
-    # Clean up temp files
-    rm -Rf "${ETC_HOSTS_FILE}" "${ETC_FSTAB_FILE}" "${VOLUME_CREATION_SCRIPT}"
-    touch "${ETC_HOSTS_FILE}" "${ETC_FSTAB_FILE}" "${VOLUME_CREATION_SCRIPT}"
 
     check_all_mandatory_parameters "${MANDATORY_PARAMETER_LIST[@]}"
     display_settings
-
-    # Initializing the script glusterfs_volume_creation_script.sh
-    cat << SCRIPT_EOF >> "${VOLUME_CREATION_SCRIPT}"
-#!/bin/bash
-
-gluster volume create ${VOLUME_NAME} replica 3 \\
-SCRIPT_EOF
 
     log_info "Installing required packages"
     apt-get install -qq -y dnsutils
@@ -108,108 +94,19 @@ SCRIPT_EOF
             if [[ "$HOSTNAME" =~ ${MANAGER_HOSTNAME_PREFIX} ]]; then
                 log_progress_bar "🔍 Testing ${IP} ..." "$((i + 1))" 254 " => Found the candidate ${HOSTNAME} (${IP})"
 
-                DISCOVERED_IPS+=("${IP}")
-            
-                echo "${IP} ${HOSTNAME}" >>"${ETC_HOSTS_FILE}"
-                echo "${HOSTNAME}:/$VOLUME_NAME  ${GLUSTER_DIR}/${COUNTER}  glusterfs  defaults,_netdev  0  0" >> "${ETC_FSTAB_FILE}"
-                printf "%s" "${HOSTNAME}:${GLUSTER_DIR}/${COUNTER}/brick " >> "${VOLUME_CREATION_SCRIPT}"
+                DISCOVERED_HOSTNAME_LISt+=("${HOSTNAME}")
                 
                 COUNTER=$((COUNTER + 1))
             fi
         fi
     done
 
-    printf "%s" " force" >> "${VOLUME_CREATION_SCRIPT}"
-
-    unset COUNTER
-
-    if [ ${#DISCOVERED_IPS[@]} -eq 0 ]; then
-        log_error "⚠️ No orchestrator nodes found. Aborting setup."
-    else
-        log_info "\n\nSetting up GlusterFS on discovered hosts: ${DISCOVERED_IPS[*]}"
-        COUNTER=1
-
-        for IP_INDEX in "${DISCOVERED_IPS[@]}"; do
-            remove_ssh_host "${IP_INDEX}"
-
-            log_warning "\t\t- Declaring the GlusterFS member to /etc/hosts"
-            copy_file_to_host "${LOGIN}" "${PASSWORD}" "${IP_INDEX}" "${ETC_HOSTS_FILE}" "/tmp"
-            sshpass -p "${PASSWORD}" ssh -o StrictHostKeyChecking=no "${LOGIN}@${IP_INDEX}" "sudo tee -a /etc/hosts < /tmp/$(basename ${ETC_HOSTS_FILE})"
-
-            log_warning "\t\t- Declaring the GlusterFS member to /etc/fstab"
-            copy_file_to_host "${LOGIN}" "${PASSWORD}" "${IP_INDEX}" "${ETC_FSTAB_FILE}" "/tmp"
-            sshpass -p "${PASSWORD}" ssh -o StrictHostKeyChecking=no "${LOGIN}@${IP_INDEX}" "sudo tee -a /etc/fstab < /tmp/$(basename ${ETC_FSTAB_FILE})"
-
-            log_warning "\t\t- Installing GlusterFS on ${IP_INDEX}"
-            sshpass -p "$PASSWORD" ssh -o StrictHostKeyChecking=no "$LOGIN@$IP_INDEX" <<EOF_GLUSTERFS
-    export DEBIAN_FRONTEND=noninteractive
-    export DEBCONF_NOWARNINGS=yes 
-
-    sudo mkdir -p "${GLUSTER_DIR}/${COUNTER}"
-
-    sudo -E apt update -qq
-    sudo -E apt install glusterfs-server -y -qq \
-         -o Dpkg::Progress-Fancy="0" \
-         -o Dpkg::Use-Pty="0"
-    sudo systemctl enable glusterd
-    sudo systemctl start glusterd
-
-    sudo mount -a
-    sudo mkdir -p "${GLUSTER_DIR}/${COUNTER}/brick"
-EOF_GLUSTERFS
-
-            COUNTER=$((COUNTER + 1))
-        done
-
-        unset COUNTER
-
-        # Probing the peers
-        for ((INDEX = 1; INDEX < ${#DISCOVERED_IPS[@]}; INDEX++)); do
-            IP_INDEX="${DISCOVERED_IPS[$INDEX]}"
-            log_debug "\t\t- Probing host ${IP_INDEX} ..."
-            sshpass -p "${PASSWORD}" ssh "${LOGIN}@${DISCOVERED_IPS[0]}" "sudo gluster peer probe ${IP_INDEX}"
-            sleep 5
-        done
-
-        # Waiting for the ppers
-        log_info "⏳ Waiting for peers to join trusted pool..."
-        sleep 5
-        sshpass -p "${PASSWORD}" ssh "${LOGIN}@${DISCOVERED_IPS[0]}" "sudo gluster peer status"        
-
-        # Creating the volume
-        log_debug "\t\t- Setting up th GlusterFS volume with the following script: ${VOLUME_CREATION_SCRIPT}"
-        log_warning "\t\t\t- Creating the volume ${VOLUME_NAME}"
-        copy_file_to_host "${LOGIN}" "${PASSWORD}" "${DISCOVERED_IPS[0]}" "${VOLUME_CREATION_SCRIPT}" "/tmp"
-        sshpass -p "${PASSWORD}" ssh -o StrictHostKeyChecking=no "${LOGIN}@${DISCOVERED_IPS[0]}" "sudo bash /tmp/$(basename ${VOLUME_CREATION_SCRIPT})"
-        log_warning "\t\t\t- Starting the volume ${VOLUME_NAME}"
-        sshpass -p "${PASSWORD}" ssh "${LOGIN}@${DISCOVERED_IPS[0]}" "sudo gluster volume start ${VOLUME_NAME}"
-        log_warning "\t\t\t- Status of the volume ${VOLUME_NAME}"
-        sshpass -p "${PASSWORD}" ssh "${LOGIN}@${DISCOVERED_IPS[0]}" "sudo gluster volume status ${VOLUME_NAME}"
-        log_warning "\t\t\t- Info of the volume ${VOLUME_NAME}"
-        sshpass -p "${PASSWORD}" ssh "${LOGIN}@${DISCOVERED_IPS[0]}" "sudo gluster volume info ${VOLUME_NAME}"
-        log_warning "\t\t\t- Setup security and authentication for the volume ${VOLUME_NAME}"
-        sshpass -p "${PASSWORD}" ssh "${LOGIN}@${DISCOVERED_IPS[0]}" "sudo gluster volume set ${VOLUME_NAME} auth.allow $(IFS=, ; echo "${DISCOVERED_IPS[*]}")"
-
-        # Mount the glusterFS volume where applications can access the files
-        log_info "Mounting the GlusterFS volume ${VOLUME_NAME} on all nodes" 
-        for IP_INDEX in "${DISCOVERED_IPS[@]}"; do
-            log_debug "\t\t- Mounting the GlusterFS volume ${VOLUME_NAME} on ${IP_INDEX}"
-            sshpass -p "${PASSWORD}" ssh -o StrictHostKeyChecking=no "${LOGIN}@${IP_INDEX}" \
-                    "echo \"localhost:/${VOLUME_NAME} /mnt glusterfs defaults,_netdev,backupvolfile-server=localhost 0 0\" | sudo tee -a /etc/fstab > /dev/null"
-            sshpass -p "${PASSWORD}" ssh -o StrictHostKeyChecking=no "${LOGIN}@${IP_INDEX}" "sudo mount.glusterfs localhost:/${VOLUME_NAME} /mnt"
-            sshpass -p "${PASSWORD}" ssh -o StrictHostKeyChecking=no "${LOGIN}@${IP_INDEX}" "df -Th"
-        done
-
-        # Testing
-        log_info "\t\t- Testing the GlusterFS volume ${VOLUME_NAME} on all nodes"
-        sshpass -p "${PASSWORD}" ssh -o StrictHostKeyChecking=no "${LOGIN}@${DISCOVERED_IPS[0]}" "echo 'Hello World!' | sudo tee /mnt/test.txt"
-
-        for IP_INDEX in "${DISCOVERED_IPS[@]}"; do
-            log_info "\t\t- Checking test file on host ${IP_INDEX} ..."
-            sshpass -p "${PASSWORD}" ssh "${LOGIN}@${IP_INDEX}" "cat /mnt/test.txt"
-            sleep 5
-        done
-    fi
+    setup_replicated_volumes \
+        "${LOGIN}" \
+        "${PASSWORD}" \
+        "${VOLUME_NAME}" \
+        "glusterfs-data" \
+        "${DISCOVERED_HOSTNAME_LISt[@]}"
 }
 
 # === Track and report duration ===
