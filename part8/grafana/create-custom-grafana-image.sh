8fb5d67c60392dbb87f8ea43a5e3d5584c04166a
Fabrice TRAN-XUAN <fabrice.tranxuan@fr.enersys.com>
2025-09-29 14:46:30 +0200

    Fixed issues when build the Grafana builder

--- part8/grafana/create-custom-grafana-image.sh
+++ part8/grafana/create-custom-grafana-image.sh
@@ -151,18 +151,18 @@ fi
 
 # Exporting the Grafana resources
 GRIZZLY_BASEDIR="$(mktemp -d)/grizzly"
+GRAFANA_CONTAINER="grafana_builder"
 
 export_grafana_resources "${GRAFANA_URL}" "${SA_TOKEN}" "${GRIZZLY_BASEDIR}"
 
 # Install the Grafana builder
-install_and_configure_grafana_builder "${TARGET_ARCH}" "${VERSION_NUMBER}" "127.0.0.1" "3000" "${ADMIN_PASSWD}" "${GRIZZLY_BASEDIR}"
+install_and_configure_grafana_builder "${TARGET_ARCH}" "${VERSION_NUMBER}" "127.0.0.1" "3000" "${ADMIN_PASSWD}" "${GRAFANA_CONTAINER}" "${GRIZZLY_BASEDIR}"
 
 # Import the Grizzly's exported resources
 import_grafana_resources "http://127.0.0.1:3000" "$(cat ${GRIZZLY_BASEDIR}/builder_sa_token.txt)" "${GRIZZLY_BASEDIR}"
 
 # Use loopback to avoid docker-for-mac/iptables oddities
 TARGET_IMAGE="${LOCAL_REGISTRY_ADDRESS}:${LOCAL_REGISTRY_PORT}/grafana:${IMAGE_VERSION}"
-GRAFANA_CONTAINER="grafana_builder"
 
 # --------------------------------------
 # Copy grafana.db out of the container
