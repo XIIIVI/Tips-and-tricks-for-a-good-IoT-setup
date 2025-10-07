090b2eda26c598c8a867b82d19f6f312d7d6dd60
Fabrice TRAN-XUAN <fabrice.tranxuan@fr.enersys.com>
2025-10-07 17:11:25 +0200

    Trying to fix issues when launching Loki

--- commons/commons-docker.sh
+++ commons/commons-docker.sh
@@ -65,8 +65,9 @@ update_docker_repo() {
     echo 'Backup saved as $BAK'
     sudo sed -E -i.bak 's|$SEARCH|$REPLACE|g' '$SRC'
     echo 'Replaced bookworm→bullseye in $SRC'
-    sudo apt clean
-    sudo rm -rf /var/lib/apt/lists/*
+    sudo apt clean > /dev/null 2>&1
+    sudo rm -rf /var/lib/apt/lists/* > /dev/null 2>&1
+    sudo apt update > /dev/null 2>&1
     sudo apt-get update -o Acquire::Retries=3
   " && echo "Update complete on $HOST_ARG"
 }
